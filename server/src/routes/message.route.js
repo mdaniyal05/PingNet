698f0cac0127a842716f82464ffd0e1857fd6363
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-09-19 17:57:20 +0500

    Add multer middleware in send message route

--- server/src/routes/message.route.js
+++ server/src/routes/message.route.js
@@ -5,8 +5,17 @@ const {
   getMessages,
   sendMessage,
 } = require("../controllers/message.controller");
+const upload = require("../middlewares/multer.middleware");
 
 router.get("/:_id", verifyJWT, getMessages);
-router.post("/send-message/:_id", verifyJWT, sendMessage);
+router.post(
+  "/send-message/:_id",
+  upload.fields([
+    { name: "image", maxCount: 1 },
+    { name: "video", maxCount: 1 },
+  ]),
+  verifyJWT,
+  sendMessage
+);
 
 module.exports = router;
