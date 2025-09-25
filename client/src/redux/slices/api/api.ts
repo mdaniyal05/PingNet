44c664d431de46fbb4bc6f7b147e653f4a6a501e
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-09-25 09:14:25 +0500

    Create api slice for rtk query + Add api slice in store

---
+++ client/src/redux/slices/api/api.ts
@@ -0,0 +1,16 @@
+import { createApi, fetchBaseQuery } from "@reduxjs/toolkit/query/react";
+import type { RootState } from "../../store";
+
+export const api = createApi({
+  baseQuery: fetchBaseQuery({
+    baseUrl: "",
+    prepareHeaders: (headers, { getState }) => {
+      const token = (getState() as RootState).auth.token;
+
+      if (!token) return headers;
+
+      headers.set("authorization", `Bearer ${token}`);
+    },
+  }),
+  endpoints: () => ({}),
+});
--- client/src/redux/store.ts
+++ client/src/redux/store.ts
@@ -1,7 +1,13 @@
 import { configureStore } from "@reduxjs/toolkit";
+import { api } from "./slices/api/api";
 
 export const store = configureStore({
-  reducer: {},
+  reducer: {
+    [api.reducerPath]: api.reducer,
+  },
+  middleware: (getDefaultMiddleware) =>
+    getDefaultMiddleware().concat(api.middleware),
+  devTools: import.meta.env.PROD ? true : false,
 });
 
 export type RootState = ReturnType<typeof store.getState>;
