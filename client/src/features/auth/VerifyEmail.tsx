c15fb7692d813affc3c1caabfdfdd03c9682652b
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-09-25 22:50:12 +0500

    Make verify email switch between otp and send email

--- client/src/features/auth/VerifyEmail.tsx
+++ client/src/features/auth/VerifyEmail.tsx
@@ -2,12 +2,18 @@ import { cn } from "@/lib/utils";
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import Otp from "@/components/Otp";
+import { useState } from "react";
+import { Input } from "@/components/ui/input";
+import { Label } from "@/components/ui/label";
 
 export default function VerifyEmail({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-  return (
+  const [email, setEmail] = useState<string>("");
+  const [isEmailSent, setIsEmailSent] = useState<boolean>(false);
+
+  return isEmailSent ? (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-1">
@@ -22,6 +28,36 @@ export default function VerifyEmail({
               <Otp />
               <Button type="submit" className="w-full">
                 Verify
+              </Button>
+            </div>
+          </form>
+        </CardContent>
+      </Card>
+    </div>
+  ) : (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-1">
+          <form className="p-6 md:p-8">
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Register your account</h1>
+                <p className="text-muted-foreground text-balance">
+                  Enter your email below to register your account
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="email">Email</Label>
+                <Input
+                  id="email"
+                  name="email"
+                  type="email"
+                  placeholder="m@example.com"
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                Send code
               </Button>
             </div>
           </form>
