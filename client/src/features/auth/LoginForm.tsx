87e5c50376a2b99de0604265499efdfff7af828f
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-09-27 19:28:20 +0500

    Add logic for login

--- client/src/features/auth/LoginForm.tsx
+++ client/src/features/auth/LoginForm.tsx
@@ -3,17 +3,52 @@ import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { Input } from "@/components/ui/input";
 import { Label } from "@/components/ui/label";
-import { Link } from "react-router";
+import { Link, useNavigate } from "react-router";
+import { useLoginMutation } from "../../app/services/authApi";
+import type { LoginRequest } from "@/types/authTypes";
+import React, { useState } from "react";
+
+type loginWithtype = "email" | "username";
 
 export default function LoginForm({
   className,
   ...props
 }: React.ComponentProps<"div">) {
-  return (
+  const [loginWith, setLoginWith] = useState<loginWithtype>("email");
+
+  const [formState, setFormState] = useState<LoginRequest>({
+    username: "",
+    email: "",
+    password: "",
+  });
+
+  const navigate = useNavigate();
+
+  const [login, { isLoading }] = useLoginMutation();
+
+  const handleChange = ({
+    target: { name, value },
+  }: React.ChangeEvent<HTMLInputElement>) =>
+    setFormState((prev) => ({ ...prev, [name]: value }));
+
+  const submitHandlerLogin = async (
+    event: React.FormEvent<HTMLFormElement>
+  ) => {
+    try {
+      event.preventDefault();
+      const response = await login({ ...formState });
+      console.log(response);
+      navigate("/chat");
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  return loginWith === "email" ? (
     <div className={cn("flex flex-col gap-6", className)} {...props}>
       <Card className="overflow-hidden p-0">
         <CardContent className="grid p-0 md:grid-cols-2">
-          <form className="p-6 md:p-8">
+          <form className="p-6 md:p-8" onSubmit={submitHandlerLogin}>
             <div className="flex flex-col gap-6">
               <div className="flex flex-col items-center text-center">
                 <h1 className="text-2xl font-bold">Welcome back</h1>
@@ -26,7 +61,9 @@ export default function LoginForm({
                 <Input
                   id="email"
                   type="email"
+                  name="email"
                   placeholder="m@example.com"
+                  onChange={handleChange}
                   required
                 />
               </div>
@@ -40,10 +77,16 @@ export default function LoginForm({
                     Forgot your password?
                   </a>
                 </div>
-                <Input id="password" type="password" required />
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  required
+                />
               </div>
               <Button type="submit" className="w-full">
-                Login
+                {isLoading ? "Loading..." : "Login"}
               </Button>
               <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
                 <span className="bg-card text-muted-foreground relative z-10 px-2">
@@ -58,6 +101,86 @@ export default function LoginForm({
                 >
                   Sign up
                 </Link>
+              </div>
+              <div className="text-center text-sm">
+                Login with{" "}
+                <Button onClick={() => setLoginWith("username")}>
+                  username
+                </Button>
+              </div>
+            </div>
+          </form>
+          <div className="bg-muted relative hidden md:block">
+            <img
+              src="/signin-image.jpg"
+              alt="signin-image"
+              className="absolute inset-0 h-full w-full object-cover dark:brightness-[0.2] dark:grayscale"
+            />
+          </div>
+        </CardContent>
+      </Card>
+    </div>
+  ) : (
+    <div className={cn("flex flex-col gap-6", className)} {...props}>
+      <Card className="overflow-hidden p-0">
+        <CardContent className="grid p-0 md:grid-cols-2">
+          <form className="p-6 md:p-8" onSubmit={submitHandlerLogin}>
+            <div className="flex flex-col gap-6">
+              <div className="flex flex-col items-center text-center">
+                <h1 className="text-2xl font-bold">Welcome back</h1>
+                <p className="text-muted-foreground text-balance">
+                  Login to your Chattrix account
+                </p>
+              </div>
+              <div className="grid gap-3">
+                <Label htmlFor="username">Username</Label>
+                <Input
+                  id="username"
+                  type="text"
+                  name="username"
+                  placeholder="m@example.com"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <div className="grid gap-3">
+                <div className="flex items-center">
+                  <Label htmlFor="password">Password</Label>
+                  <a
+                    href="#"
+                    className="ml-auto text-sm underline-offset-2 hover:underline"
+                  >
+                    Forgot your password?
+                  </a>
+                </div>
+                <Input
+                  id="password"
+                  type="password"
+                  name="password"
+                  onChange={handleChange}
+                  required
+                />
+              </div>
+              <Button type="submit" className="w-full">
+                {isLoading ? "Loading..." : "Login"}
+              </Button>
+              <div className="after:border-border relative text-center text-sm after:absolute after:inset-0 after:top-1/2 after:z-0 after:flex after:items-center after:border-t">
+                <span className="bg-card text-muted-foreground relative z-10 px-2">
+                  Or
+                </span>
+              </div>
+              <div className="text-center text-sm">
+                Don&apos;t have an account?{" "}
+                <Link
+                  to="/auth/verify"
+                  className="underline underline-offset-4"
+                >
+                  Sign up
+                </Link>
+              </div>
+              <div className="text-center text-sm">
+                Login with{" "}
+                <Button onClick={() => setLoginWith("email")}>email</Button>
               </div>
             </div>
           </form>
