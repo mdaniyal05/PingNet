b99e345ad35fc2a924cb1e0d613cf28ba86a4088
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-10-07 09:47:57 +0500

    Create messages api mutations and queries

---
+++ client/src/app/services/messageApi.ts
@@ -0,0 +1,31 @@
+import { api } from "../services/api";
+
+const API_URL: string = import.meta.env.VITE_API_URL;
+
+let MESSAGE_URL: string;
+
+if (import.meta.env.PROD) {
+  MESSAGE_URL = `${API_URL}/api/v1/messages`;
+} else {
+  MESSAGE_URL = "/api/v1/messages";
+}
+
+export const messageApi = api.injectEndpoints({
+  endpoints: (builder) => ({
+    getMessages: builder.query({
+      query: (_id) => ({
+        url: `${MESSAGE_URL}/message/${_id}`,
+        method: "GET",
+      }),
+    }),
+    sendMessage: builder.mutation({
+      query: ({ _id, ...payload }) => ({
+        url: `${MESSAGE_URL}/message/send-message/${_id}`,
+        method: "POST",
+        body: payload,
+      }),
+    }),
+  }),
+});
+
+export const { useGetMessagesQuery, useSendMessageMutation } = messageApi;
