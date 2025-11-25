cfb44518c8d8dfc9c48f735bdcf84fe3730a5ebe
Daniyal <154412066+mdaniyal05@users.noreply.github.com>
2025-11-25 22:24:06 +0500

    Refactor + Fix resend service

--- server/src/service/resend.js
+++ server/src/service/resend.js
@@ -4,7 +4,7 @@ const resend = new Resend(process.env.RESEND_API_KEY);
 
 const mailSender = async (email, subject, text, html) => {
   try {
-    const mail = resend.emails.send({
+    const { data, error } = await resend.emails.send({
       from: process.env.FROM_EMAIL,
       to: email,
       subject: subject,
@@ -12,7 +12,11 @@ const mailSender = async (email, subject, text, html) => {
       html: html,
     });
 
-    console.log(mail);
+    if (error) {
+      console.error(error);
+    }
+
+    console.log(data);
   } catch (error) {
     console.error(error);
   }
